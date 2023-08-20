304e2f9dce3cb112b47cf9adbf63e4137c47f5fc
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-08-21 01:07:38 +0500

    Add authentication

--- middleware.js
+++ middleware.js
@@ -1,3 +1,15 @@
 export { default } from "next-auth/middleware";
 
-export const config ={matcher:['/dashboard']}
+export const config = {
+  matcher: [
+    "/dashboard",
+    "/dashboard/add-project",
+    "/dashboard/show-projects",
+    "/dashboard/add-skill",
+    "/dashboard/show-skills",
+    "/dashboard/add-framework",
+    "/dashboard/show-frameworks",
+    "/dashboard/add-language",
+    "/dashboard/show-languages",
+  ],
+};
